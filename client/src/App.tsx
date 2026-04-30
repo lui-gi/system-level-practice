c09e5014028b18458bfd5aba68a08ec3959e0c80
Luigi <luigi.fern05@gmail.com>
2026-04-29 20:38:41 -0400

    fix: use dynamic /:section routes so useParams populates section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,21 +3,16 @@ import Layout from "./components/Layout.tsx";
 import HomePage from "./pages/HomePage.tsx";
 import SectionPage from "./pages/SectionPage.tsx";
 import SlidesPage from "./pages/SlidesPage.tsx";
-import { SECTIONS } from "./lib/sections.ts";
 
 export default function App() {
-  const sectionRoutes = SECTIONS.flatMap((s) => [
-    <Route key={`${s.id}-list`} path={`/${s.id}`} element={<SectionPage />} />,
-    <Route key={`${s.id}-detail`} path={`/${s.id}/:id`} element={<SectionPage />} />,
-  ]);
-
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {sectionRoutes}
         <Route path="/slides" element={<SlidesPage />} />
         <Route path="/slides/:filename" element={<SlidesPage />} />
+        <Route path="/:section" element={<SectionPage />} />
+        <Route path="/:section/:id" element={<SectionPage />} />
       </Routes>
     </Layout>
   );
