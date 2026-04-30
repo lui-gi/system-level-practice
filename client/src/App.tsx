ee3d56bb57d9cd2666059a6b3d1294bbb3f29c9c
Luigi <luigi.fern05@gmail.com>
2026-04-29 20:18:22 -0400

    feat: route /:section and /:section/:id through SectionPage; remove PracticePage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,21 @@
 import { Routes, Route } from "react-router";
 import Layout from "./components/Layout.tsx";
 import HomePage from "./pages/HomePage.tsx";
-import PracticePage from "./pages/PracticePage.tsx";
+import SectionPage from "./pages/SectionPage.tsx";
 import SlidesPage from "./pages/SlidesPage.tsx";
+import { SECTIONS } from "./lib/sections.ts";
 
 export default function App() {
+  const sectionRoutes = SECTIONS.flatMap((s) => [
+    <Route key={`${s.id}-list`} path={`/${s.id}`} element={<SectionPage />} />,
+    <Route key={`${s.id}-detail`} path={`/${s.id}/:id`} element={<SectionPage />} />,
+  ]);
+
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/practice" element={<PracticePage />} />
-        <Route path="/practice/:id" element={<PracticePage />} />
+        {sectionRoutes}
         <Route path="/slides" element={<SlidesPage />} />
         <Route path="/slides/:filename" element={<SlidesPage />} />
       </Routes>
